5ab44a5a086d6c6ae1f2a68ce324e25980554265
Marley William Byers <professionalmarley@gmail.com>
2025-04-06 04:23:19 -0700

    Made specialTrait fields transfer so they maintain data on drop and pickup, and fixed null effect issue

--- Assets/Scripts/Items/ItemController.cs
+++ Assets/Scripts/Items/ItemController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 public class ItemController : MonoBehaviour
@@ -34,10 +35,34 @@ public class ItemController : MonoBehaviour
             c.gameObject.AddComponent<ItemSpawner>().items = new List<ItemSpawner.Spawnable> 
                 {new ItemSpawner.Spawnable {itemSpawn = prefab, weight = 1}};
             c.AddStatChange(info.statChanges);
-            foreach (Effect e in info.Effects) {c.AddAttackEffect(e);}
+            foreach (Effect e in info.Effects) 
+            {
+                if (e.level == 0)
+                {
+                    Debug.LogError("Effect level is 0 for " + info.itemName);
+                } else if (e.duration == 0)
+                {
+                    Debug.LogWarning("Effect duration is 0 for " + info.itemName);
+                } else if (e.particles == null)
+                {
+                    Debug.LogWarning("Effect particles is null for " + info.itemName);
+                }
+                c.AddAttackEffect(e);
+            }
             foreach (SpecialTrait trait in GetComponents<SpecialTrait>())
             {
                 SpecialTrait addedTrait = c.gameObject.AddComponent(trait.GetType()) as SpecialTrait;
+                // Copy all the fields over and then set the user to the character who got it
+
+                // Get all instance fields (public and non-public)
+                FieldInfo[] fields = trait.GetType().GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+                foreach (FieldInfo field in fields)
+                {
+                    // Copy the field value from the original trait to the new trait
+                    object value = field.GetValue(trait);
+                    field.SetValue(addedTrait, value);
+                }
+
                 addedTrait.user = c;
             }
 
